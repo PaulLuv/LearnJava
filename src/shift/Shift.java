a9513ad64ba37a0b5d01c446267fd16e4d7aea6f
Paul Luv <904824805@qq.com>
2018-06-23 10:09:44 +0800

    add shift operation result

--- src/shift/Shift.java
+++ src/shift/Shift.java
@@ -7,9 +7,9 @@ public class Shift {
     private static int c = 98;
 
     public static void main(String[] arg){
-        System.out.println("100 >> 1 = " + (a >> 1));
-        System.out.println("99 >> 1 = " + (b>>1));
-        System.out.println("98 >> 1 = " + (c>>1));
+        System.out.println("100 >> 2 = " + (a >> 2)); // 25
+        System.out.println("99 >> 1 = " + (b>>1)); // 49
+        System.out.println("98 >> 1 = " + (c>>1)); // 49
     }
 
 }
