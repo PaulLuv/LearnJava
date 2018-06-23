673b94a1559ff799623465b2cd10ba4cf24fbdcf
Paul Luv <904824805@qq.com>
2018-06-23 10:08:43 +0800

    add shift operation

---
+++ src/shift/Shift.java
@@ -0,0 +1,15 @@
+package shift;
+
+public class Shift {
+
+    private static int a = 100;
+    private static int b = 99;
+    private static int c = 98;
+
+    public static void main(String[] arg){
+        System.out.println("100 >> 1 = " + (a >> 1));
+        System.out.println("99 >> 1 = " + (b>>1));
+        System.out.println("98 >> 1 = " + (c>>1));
+    }
+
+}
